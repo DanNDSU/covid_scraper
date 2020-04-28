ac6e53338bbff8cdd14e92497feabe346d913b16
qianyaoyy <47509255+qianyaoyy@users.noreply.github.com>
2020-04-28 17:55:34 -0500

    server problem solved!
    from server side send lists of html and related texts on this html by coomparing with keywords.

--- server0428.py
+++ server0428.py
@@ -6,65 +6,64 @@ import requests
 import re
 import pickle
 
+import sys
+sys.setrecursionlimit(10 ** 8)
+
 def WebCrawl(url):
 	try:
 		page = requests.get(url)
 		soup = BeautifulSoup(page.text, 'html.parser')
-
-		# prints any titles from the first page hit.
-		title = soup.find_all('title')
+		#title = soup.find_all('title')
 		links = soup.find_all('a', attrs={'href': re.compile("^http")})
 
 		tf_results = []
-		results = []
-		add_list = []
+		url_list = []
+		target=[]
 		keyword1='covid-19'
 		keyword2='coronavirus'
-
 		for link in links:
 			address = link.get('href')
 			try:
-				new_page = requests.get(address)
-				new_soup = BeautifulSoup(new_page.text, 'html.parser')
-				title2 = new_soup.find_all('title')
-				texts2 = new_soup.find_all('p')
-				links2=new_soup.find_all('a', attrs={'href': re.compile("^http")})
-
+				# get texts of address to decide whether this address is about covid - 19
+				page2 = requests.get(address)
+				soup2 = BeautifulSoup(page2.text, 'html.parser')
+				#title2 = soup2.find_all('title')
+				#links2=soup2.find_all('a', attrs={'href': re.compile("^http")})
+				texts2 = soup2.find_all('p')
+                # loop through texts
 				for idx in range(len(texts2)):
 					L = texts2[idx].get_text()
 					tf_value = (keyword1.lower() in L.lower()) or (keyword2.lower() in L.lower())
 					tf_results.append(tf_value)
-
 					if tf_value == True:
-						results.append(texts2[idx])
+						target.append(L)
 			except Exception as ie:
 				pass
 
 			if any(True == s for s in tf_results):
-				add_list.append([address,results])
+				url_list.append([address,target])
 
 	except Exception as ie:
 		pass
 
-	return add_list
+	return url_list
 
 
 print_lock = threading.Lock()
 
 def threaded(c):
-	HEADERSIZE = 10
+	HEADERSIZE = 100
 	while True:
-		url = c.recv(4096)
+		url = c.recv(40960)
 		if not url:
 			print('Bye')
 			print_lock.release()
 			break
 		my_url=url.decode('utf-8')
-		print('the website you are crawling is: ')
-		print(my_url)
+		print('the website you are crawling is: ', my_url)
 		url_list= WebCrawl(my_url)
-		print('crawled links #. is : ')
-		print(len(url_list))
+		print('crawled links #. is : ', len(url_list))
+
 		msg=pickle.dumps(url_list)
 		msg = bytes(f"{len(msg):<{HEADERSIZE}}", 'utf-8')+msg
 		c.send(msg)
